98ba832e58a464152d38e06453eea20eec86047a
Kapil Chaudhary <kc685838@gmail.com>
2023-05-10 14:15:45 +0530

    submitting code-Wed May 10 2023 14:15:42 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,16 @@ import React from "react";
 import './../styles/App.css';
 
 const App = () => {
+  let count = 0;
+  function btnClicked() {
+    count++;
+    document.getElementById("display").innerHTML = `Button clicked ${count} times`;
+  }
   return (
     <div>
         {/* Do not remove the main div */}
+        <p id="display">Button clicked 0 times.</p>
+        <button onClick={btnClicked}>Click me</button>
     </div>
   )
 }
